412120971684ed3aa08e088ffcee823e27463cce
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2022-04-17 11:19:08 +0200

    Add a chain of buttons

--- ColorMyViews/app/src/main/java/com/androidera/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/androidera/colormyviews/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +23,15 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.red_button -> findViewById<View>(R.id.box_three_text).setBackgroundResource(
+                R.color.orange_900
+            )
+            R.id.yellow_button -> findViewById<View>(R.id.box_four_text).setBackgroundResource(
+                R.color.amber_500
+            )
+            R.id.green_button -> findViewById<View>(R.id.box_five_text).setBackgroundResource(
+                R.color.green_a700
+            )
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
@@ -33,11 +43,16 @@ class MainActivity : AppCompatActivity() {
         val boxFourText = findViewById<TextView>(R.id.box_four_text)
         val boxFiveText = findViewById<TextView>(R.id.box_five_text)
 
+        val redButton = findViewById<Button>(R.id.red_button)
+        val greenButton = findViewById<Button>(R.id.green_button)
+        val yellowButton = findViewById<Button>(R.id.yellow_button)
+
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
 
         val clickableViews: List<View> = listOf(
             boxOneText, boxTwoText, boxThreeText,
-            boxFourText, boxFiveText, rootConstraintLayout
+            boxFourText, boxFiveText, rootConstraintLayout,
+            redButton, greenButton, yellowButton
         )
 
         for (view in clickableViews) {
