4380b91f02ced1e75f1d1cbd52006cc2e5c626a0
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2022-04-19 19:38:36 +0200

    Add a fragment

---
+++ AndroidTrivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,32 @@
+package com.example.android.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+class TitleFragment : Fragment() {
+
+    private var _binding: FragmentTitleBinding? = null
+    private val binding: FragmentTitleBinding = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(
+            layoutInflater, R.layout.fragment_title, container, false
+        )
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
+
