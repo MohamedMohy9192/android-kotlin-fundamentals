c5b8f3bc6ea9b918e1aff0dc26193d8770a9be15
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2022-04-20 15:26:43 +0200

    Add navigation components to the project

--- AndroidTrivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ AndroidTrivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,11 +1,12 @@
 package com.example.android.navigation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -22,6 +23,14 @@ class TitleFragment : Fragment() {
         )
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.playButton.setOnClickListener {
+            findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }
     }
 
     override fun onDestroyView() {
