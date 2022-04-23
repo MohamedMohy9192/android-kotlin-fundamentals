93c74e2123d1c4accd63886dc2a57e6793383b19
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2022-04-23 17:27:04 +0200

    Update MainActivity.kt

--- AndroidTrivia/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ AndroidTrivia/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.android.navigation.databinding.ActivityMainBinding
@@ -31,6 +32,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
     private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,7 +45,8 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.findNavController()
-        setupActionBarWithNavController(navController, drawerLayout)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
         NavigationUI.setupWithNavController(binding.navView, navController)
     }
 
@@ -58,7 +61,7 @@ class MainActivity : AppCompatActivity() {
     // R.layout.fragment_title
 
     override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(navController, drawerLayout) ||
+        return NavigationUI.navigateUp(navController, appBarConfiguration) ||
                 super.onSupportNavigateUp()
     }
 }
