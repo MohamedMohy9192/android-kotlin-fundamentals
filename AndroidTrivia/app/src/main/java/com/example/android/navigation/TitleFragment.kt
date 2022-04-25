3bef8b9837695746a6e387cc5e76bfa5c05e52a5
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2022-04-25 23:12:39 +0200

    Setup Timber for logging

--- AndroidTrivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ AndroidTrivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,5 +1,6 @@
 package com.example.android.navigation
 
+import android.content.Context
 import android.os.Bundle
 import android.view.*
 import androidx.databinding.DataBindingUtil
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
+import timber.log.Timber
 
 class TitleFragment : Fragment() {
 
@@ -16,6 +18,7 @@ class TitleFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
+        Timber.i("onCreate called")
     }
 
     override fun onCreateView(
@@ -26,13 +29,13 @@ class TitleFragment : Fragment() {
             layoutInflater, R.layout.fragment_title, container, false
         )
         // Inflate the layout for this fragment
-
+        Timber.i("onCreateView called")
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        Timber.i("onCreate onViewCreated")
         binding.playButton.setOnClickListener {
             findNavController().navigate(
                 TitleFragmentDirections.actionTitleFragmentToGameFragment()
@@ -52,7 +55,44 @@ class TitleFragment : Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
+        Timber.i("onDestroyView called")
         _binding = null
     }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Timber.i("onAttach called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.i("onResume called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Timber.i("onStart called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Timber.i("onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Timber.i("onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.i("onDestroy called")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Timber.i("onDetach called")
+    }
+    
 }
 
