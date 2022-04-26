8d181a4d32e71e8d98b9ff52ee8d419b134ec890
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2022-04-26 04:32:52 +0200

    Update Gradle dependencies

--- AndroidTrivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ AndroidTrivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -35,7 +35,7 @@ class TitleFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Timber.i("onCreate onViewCreated")
+        Timber.i("onViewCreated called")
         binding.playButton.setOnClickListener {
             findNavController().navigate(
                 TitleFragmentDirections.actionTitleFragmentToGameFragment()
