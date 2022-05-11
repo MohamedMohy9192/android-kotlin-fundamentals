c548c651aa9aeb884dd9c2beae1949be70184314
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2022-05-11 20:49:47 +0200

    Add buzzer

--- GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -8,7 +8,20 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
+// Vibration is controlled by passing in an array representing the number of milliseconds
+// each interval of buzzing and non-buzzing takes.
+// So the array [0, 200, 100, 300] will wait 0 milliseconds,
+// then buzz for 200ms, then wait 100ms, then buzz fo 300ms.
+
+private val CORRECT_BUZZ_PATTERN = longArrayOf(100, 100, 100, 100, 100, 100)
+private val PANIC_BUZZ_PATTERN = longArrayOf(0, 200)
+private val GAME_OVER_BUZZ_PATTERN = longArrayOf(0, 2000)
+private val NO_BUZZ_PATTERN = longArrayOf(0)
+
 class GameViewModel : ViewModel() {
+
+    private val _buzzer = MutableLiveData<BuzzType>()
+    val buzzer: LiveData<BuzzType> get() = _buzzer
 
     // The current word
     private val _word = MutableLiveData<String>("")
@@ -20,7 +33,6 @@ class GameViewModel : ViewModel() {
         val letterAtIndex = currentWord[randomLetterIndex - 1].uppercaseChar()
         "Current word has ${currentWord.length} letters " +
                 "\nThe letter at position $randomLetterIndex is $letterAtIndex"
-
     }
 
     // The current score
@@ -90,10 +102,14 @@ class GameViewModel : ViewModel() {
             override fun onTick(millisUntilFinished: Long) {
                 // Convert the millisUntilFinished to seconds
                 _currentTime.value = millisUntilFinished / ONE_SECOND
+                if (millisUntilFinished / ONE_SECOND <= COUNTDOWN_PANIC_SECONDS) {
+                    _buzzer.value = BuzzType.COUNTDOWN_PANIC
+                }
             }
 
             override fun onFinish() {
                 _currentTime.value = DONE
+                _buzzer.value = BuzzType.GAME_OVER
                 onGameFinish()
             }
 
@@ -110,6 +126,7 @@ class GameViewModel : ViewModel() {
 
     fun onCorrect() {
         _score.value = score.value?.plus(1)
+        _buzzer.value = BuzzType.CORRECT
         nextWord()
     }
 
@@ -119,10 +136,10 @@ class GameViewModel : ViewModel() {
     private fun nextWord() {
         if (wordList.isEmpty()) {
             resetList()
-        } else {
-            //Select and remove a word from the list
-            _word.value = wordList.removeAt(0)
         }
+        //Select and remove a word from the list
+        _word.value = wordList.removeAt(0)
+
     }
 
     /**
@@ -137,6 +154,10 @@ class GameViewModel : ViewModel() {
      */
     fun onGameFinishComplete() {
         _eventGameFinish.value = false
+    }
+
+    fun onBuzzComplete() {
+        _buzzer.value = BuzzType.NO_BUZZ
     }
 
     override fun onCleared() {
@@ -154,5 +175,15 @@ class GameViewModel : ViewModel() {
 
         // Total time for the game
         private const val COUNTDOWN_TIME = 60_000L
+
+        // This is the time when the phone will start buzzing each second
+        private const val COUNTDOWN_PANIC_SECONDS = 10L
+    }
+
+    enum class BuzzType(val pattern: LongArray) {
+        CORRECT(CORRECT_BUZZ_PATTERN),
+        GAME_OVER(GAME_OVER_BUZZ_PATTERN),
+        COUNTDOWN_PANIC(PANIC_BUZZ_PATTERN),
+        NO_BUZZ(NO_BUZZ_PATTERN)
     }
 }
